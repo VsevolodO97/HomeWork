05f8c5fc2a40c110a80b44c08bae29b5feee7bec
AlexKurbatov <AlexKurbatov@icloud.com>
2021-08-11 22:08:36 +0500

    Modified file 2021-08-08_HomeWork.playground Ended Tasks 1.

--- Swift basic/2021-08-08_HomeWork.playground/Contents.swift
+++ Swift basic/2021-08-08_HomeWork.playground/Contents.swift
@@ -77,10 +77,165 @@ struct ArrayInterface {
 
 // Интерфейс односвязного списка
 
+struct SinglyLinkedList {
+    let array = Array(1...1000)
+    var element: (data: Int, next: Int)
+    
+    func insertAtEnd(array: [Int], element: Int) -> [Int] {
+        let tempArr = [element]
+        return array + tempArr
+    }
+    
+    func insertAtHead(array: [Int], element: Int) -> [Int] {
+        return []
+    }
+    
+    func delete(array: [Int]) -> [Int] {
+        return []
+    }
+    
+    func deleteAtHead(array: [Int]) -> [Int] {
+        return []
+    }
+    
+    func search(array: [Int], element: Int) -> Int {
+        return 0
+    }
+    
+    func isEmpty(array: [Int]) -> Bool {
+        if array.count == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+}
+
+// Интерфейс двусвязного списка
+
+struct DoublyLinkedList {
+    let array = Array(1...1000)
+    var element: (previous: Int, data: Int, next: Int)
+    
+    func insertAtEnd(array: [Int], element: Int) -> [Int] {
+        let tempArr = [element]
+        return array + tempArr
+    }
+    
+    func insertAtHead(array: [Int], element: Int) -> [Int] {
+        return []
+    }
+    
+    func delete(array: [Int]) -> [Int] {
+        return []
+    }
+    
+    func deleteAtHead(array: [Int]) -> [Int] {
+        return []
+    }
+    
+    func search(array: [Int], element: Int) -> Int {
+        return 0
+    }
+    
+    func isEmpty(array: [Int]) -> Bool {
+        if array.count == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+}
+
+// Интерфейс стека
+
 struct Stack {
     let array = Array(1...1000)
-
+    
+    func push(array: [Int], element: Int) -> [Int] {
+        let tempArr = [element]
+        return array + tempArr
+    }
+    
+    func pop(array: inout [Int]) -> Int {
+        var tempArr: [Int] = []
+        let lastElement = array[array.count - 1]
+        for i in array where i < array.count - 2 {
+            tempArr.append(array[i])
+        }
+        array = tempArr
+        return lastElement
+    }
+    
+    func top(array: [Int]) -> Int {
+        return array[array.count - 1]
+    }
+    
+    func isEmpty(array: [Int]) -> Bool {
+        if array.count == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
 }
+
+// Интерфейс очереди
+
+struct Queue {
+    let array = Array(1...1000)
+    
+    func enqueue(array: [Int], element: Int) -> [Int] {
+        let tempArr = [element]
+        return array + tempArr
+    }
+    
+    func dequeue(array: inout [Int]) -> Int {
+        var tempArr: [Int] = []
+        let firstElement = array[0]
+        for i in array where i > 0 {
+            tempArr.append(array[i])
+        }
+        array = tempArr
+        return firstElement
+    }
+    
+    func top(array: [Int]) -> Int {
+        return array[0]
+    }
+    
+    func isEmpty(array: [Int]) -> Bool {
+        if array.count == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+// Интерфейс хэш-таблицы
+
+struct HashTable {
+    let array = Array(1...1000)
+    
+    // Функция, проверяющая пустой  словарь или нет
+    func isEmpty() -> Bool {return true}
+    
+    // Функция, которая считает количество элементов в словаре
+    func count() -> Int {
+        let count = 0
+        return count
+    }
+    
+    // Функция доступа к элементу массива по ключу
+    func keyValue(dict: [String: Int], key: String) -> Int? {
+        let element: Int = 0
+        return element
+    }
+}
+
 
 /*
  3. Реализовать на Swift сортировку пузырьком
