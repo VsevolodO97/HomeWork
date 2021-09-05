0aaac812ad3ddf91a57af6fed421c3268807122e
AlexKurbatov <AlexKurbatov@icloud.com>
2021-09-05 12:11:25 +0500

    Modified file 2021-09-02_HomeWork.playground - added solution fot task 1

--- Swift basic/2021-09-02_HomeWork.playground/Contents.swift
+++ Swift basic/2021-09-02_HomeWork.playground/Contents.swift
@@ -59,3 +59,55 @@ ivan.recordGrade(geography)
 print(ivan.grades.map {$0.subject})
 ivan.updateGrade(Grade(subject: "Geo", position: 40, points: 30, letter: "D"))
 print(ivan.grades.filter {$0.subject == "Geo"}) // как распечатать красиво?
+
+
+/*
+ 1. Рассмотрим приложение для просмотра фильмов. Пользователи могут создавать списки фильмов и делиться этими списками с другими пользователями. Нужно создать классы User и List, который использует семантику ссылок для поддержки списков между пользователями.
+
+ - User: есть метод addList (_ :), который добавляет данный список в словарь объектов List (используя имя в качестве ключа) и list (forName 🙂 -> List?, который возвращает список для предоставленного имени.
+ - List: содержит название и массив названий фильмов. Метод print выводит все фильмы в списке.
+
+ Далее нужно создать двух пользователей. У них должны быть списки и часть списком доступна сразу двух (пользователи поделились ими). Если оба пользователя изменят один и тот же список, что будет?
+ Что произойдет, если реализовать то же самое со структурами?
+
+ */
+
+class User {
+    var userName: String
+    var filmsLibrary: [String: List] = [:]
+    
+    func addList(list: List) {
+        filmsLibrary.merge([list.listName : list]) { (current, _) in current }
+    }
+    
+    func list(forName: String) -> List? {
+        guard let _ = filmsLibrary.firstIndex(where: {$0.key == forName})  else {
+            print("\(userName) has not library of films named \(forName)")
+            return nil
+        }
+        print(filmsLibrary[forName] ?? print("\(userName) has not library of films named \(forName)"))
+        return filmsLibrary[forName]
+    }
+    
+    init(userName: String) {
+        self.userName = userName
+    //    self.filmsLibrary = filmsLibrary
+    }
+}
+
+class List {
+    var listName: String
+    var genreList: [String]
+    
+    init(listName: String, genreList: [String]) {
+        self.listName = listName
+        self.genreList = genreList
+    }
+}
+
+var horror = List(listName: "Horror", genreList: ["The katcher", "The call"])
+var vasil = User(userName: "Vasil")
+vasil.addList(list: horror)
+print(vasil.filmsLibrary)
+vasil.list(forName: "Horror")
+vasil.list(forName: "Humor")
