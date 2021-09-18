4746630315382dca4209a9e909e2a1801662d3dc
AlexKurbatov <AlexKurbatov@icloud.com>
2021-09-18 20:23:25 +0500

    Modified file 2021-09-16_HomeWork.playground - added three tasks

--- Swift basic/2021-09-16_HomeWork.playground/Contents.swift
+++ Swift basic/2021-09-16_HomeWork.playground/Contents.swift
@@ -63,3 +63,73 @@ enum Month: Int {
 Month.december.rawValue
 let currentMonth: Month = .december
 currentMonth.countMonthToSummer
+
+/*
+ 1. Есть два массива целых чисел. Нужно реализовать функцию, которая возвращает true, если каждое значение в первом списке имеет соответствующее значение, возведенное в квадрат, во втором списке. Размерность массивов  должна быть одинаковой, например. если в первом есть два значения, во втором должны существовать ровно два значения в квадрате.
+ */
+
+func compareTwoArrrays(firstArray: [Int], secondArray: [Int]) -> Bool {
+    guard firstArray.count == secondArray.count else { // Поверяем размерности массивов. Если одинаковая - продолжеам выполнение программы
+        print("Arrays could not be compared.")
+        return false
+    }
+    for i in 0...firstArray.count - 1 {
+        if secondArray[i] == Int(pow(Double(firstArray[i]), 2))  {
+            continue
+        } else {
+            print("All elements of the second array are not elements of the first array raised to the second power.")
+            return false
+        }
+    }
+    print("All elements of the second array are elements of the first array raised to the second power.")
+    return true
+}
+
+let array1 = [1, 3, 3, 5]
+let array2 = [1, 9, 9, 25]
+
+compareTwoArrrays(firstArray: array1, secondArray: array2)
+
+/*
+ 2. Определить дублированный элемент в массиве, в котором содержатся элементы типа Int от 1 до 100, при условии, что в массиве дублируется только один элемент
+ */
+
+var arrayInts = Array(1...100)
+arrayInts.insert(16, at: 35)
+
+//arrayInts.sort()
+func findEqualElements(array: [Int]) -> Int {
+    let sortedArray = array.sorted()    // Сортируем массив, чтобы одинаковые числа находились рядом друг с другом
+    var element: Int = 0
+    for index in 0...sortedArray.count - 1 {    // Организовываем цикличный поиск одинаковых элементов
+        if sortedArray[index] == sortedArray[index + 1] {
+            element = sortedArray[index]
+            break   // Прекращаем цикл поиска при нахождении двух одинаковых элементов
+        }
+    }
+    return element
+}
+
+findEqualElements(array: arrayInts)
+
+/*
+ 3. Задан массив целых чисел. Нужно создать из него два отсортированных по возрастанию массива, содержащих четные и нечетные числа
+ */
+
+func separetArray(array: [Int]) -> (evenArray: [Int], oddArray: [Int]) {
+    var evenArray: [Int] = []
+    var oddArray: [Int] = []
+    for element in array {
+        if element % 2 == 0 {
+            evenArray.append(element)
+        } else {
+            oddArray.append(element)
+        }
+    }
+    evenArray.sort()
+    oddArray.sort()
+    return (evenArray, oddArray)
+}
+
+arrayInts.sort(by: >)
+print(separetArray(array: arrayInts))
